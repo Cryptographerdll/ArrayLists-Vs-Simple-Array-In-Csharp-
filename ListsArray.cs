63bc721b909695fae0d6f33e5c394be59066c875
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-11-27 18:30:58 +0100

    Add files via upload

---
+++ ListsArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Array_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple Array In C# .
+
+            int[] Intes = new int[] { 1, 2, 3 };
+           //  Intes[3] = 99; False Output .
+
+            // Array Lists In C# .
+
+            ArrayList Ages = new ArrayList();
+            Ages.Add(1);
+            Ages.Add(2);
+            Ages.Add(3);
+            Ages.Add(4);
+
+
+            foreach (var age in Ages)
+                Console.Write(age);
+        }
+    }
+}
