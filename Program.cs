565229f5603770ecb07053188de7b417ae1c73be
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-11-27 18:27:54 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,24 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Collections.Generic;
-using System.Collections;
 
-namespace Array_Lists
+namespace Uses_OF_Lists
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Simple Array In C# .
+            List<Animal> Animales = new List<Animal>();
 
-            int[] Intes = new int[] { 1, 2, 3 };
-           //  Intes[3] = 99; False Output .
+            Animales.Add(new Animal("Panda", 25));
+            Animales.Add(new Animal("Monkey", 10));
+            Animales.Add(new Animal("Dankey", 30));
 
-            // Array Lists In C# .
-
-            ArrayList Ages = new ArrayList();
-            Ages.Add(1);
-            Ages.Add(2);
-            Ages.Add(3);
-            Ages.Add(4);
-
-
-            foreach (var age in Ages)
-                Console.Write(age);
+            foreach (var animal in Animales)
+                Console.WriteLine($"Name: {animal.name} & Age: {animal.age}");
         }
     }
 }
